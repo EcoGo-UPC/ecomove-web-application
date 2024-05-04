c5acbf7d43f95154fca7e6ea50f4e1f185d5fe84
MarianoAmes <129983231+MarianoAmes@users.noreply.github.com>
2024-05-04 05:15:41 -0500

    feat: add payment method page

--- src/public/router/router.js
+++ src/public/router/router.js
@@ -2,11 +2,13 @@ import { createWebHistory, createRouter } from 'vue-router';
 import HomePage from '../pages/home.page.vue';
 import VehiclesPage from '../../vehicle-management/pages/vehicles.page.vue';
 import InputCard from '../../payment/pages/input-card.page.vue';
+import PaymentMethodPage from '../../payment/pages/payment-method.page.vue';
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/vehículos', component: VehiclesPage },
-  { path:'/inputCard', component: InputCard }
+  { path:'/inputCard', component: InputCard },
+  { path:'/paymentmethod', component: PaymentMethodPage }
 ]
 
 const router = createRouter({
