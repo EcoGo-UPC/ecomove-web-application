5935c93c08ab0192c8279246de91b1cdbd1ad2b1
Milagros Tongo <107314451+Mily27@users.noreply.github.com>
2024-05-03 22:45:07 -0500

    feat: input card data

--- src/public/router/router.js
+++ src/public/router/router.js
@@ -1,10 +1,12 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import HomePage from '../pages/home.page.vue';
 import VehiclesPage from '../../vehicle-management/pages/vehicles.page.vue';
+import InputCard from '../../payment/pages/input-card.page.vue';
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/vehículos', component: VehiclesPage }
+  { path: '/vehículos', component: VehiclesPage },
+  { path:'/inputCard', component: InputCard }
 ]
 
 const router = createRouter({
