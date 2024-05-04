3d672bb7673e814bcf6a93668df0b82200cbe67e
Milagros Tongo <107314451+Mily27@users.noreply.github.com>
2024-05-04 06:52:03 -0500

    feat: add confirmation card

--- src/public/router/router.js
+++ src/public/router/router.js
@@ -4,13 +4,15 @@ import VehiclesPage from '../../vehicle-management/pages/vehicles.page.vue';
 import InputCard from '../../payment/pages/input-card.page.vue';
 import PaymentMethodPage from '../../payment/pages/payment-method.page.vue';
 import SubscriptionPage from "../../payment/pages/subscription.page.vue";
+import ConfirmationCardPage from "../../payment/pages/confirmation-card.page.vue";
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/vehículos', component: VehiclesPage },
   { path:'/inputCard', component: InputCard },
   { path:'/paymentmethod', component: PaymentMethodPage },
-  { path: '/subscriptions', component: SubscriptionPage }
+  { path: '/subscriptions', component: SubscriptionPage },
+  { path: '/confirmationCard', component: ConfirmationCardPage}
 ]
 const router = createRouter({
   history: createWebHistory(),
