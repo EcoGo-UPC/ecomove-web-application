477deb519547653ef7532b8c1bb10f4914727935
MarianoAmes <129983231+MarianoAmes@users.noreply.github.com>
2024-05-04 04:37:36 -0500

    feat: add reservation page

--- src/public/router/router.js
+++ src/public/router/router.js
@@ -1,10 +1,12 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import HomePage from '../pages/home.page.vue';
 import VehiclesPage from '../../vehicle-management/pages/vehicles.page.vue';
+import ReservationPage from '../../booking-reservation/pages/reservation.page.vue';
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/vehículos', component: VehiclesPage }
+  { path: '/vehículos', component: VehiclesPage },
+  { path: '/reservation', component: ReservationPage}
 ]
 
 const router = createRouter({
