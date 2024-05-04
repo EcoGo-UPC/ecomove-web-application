e2508533e87d33a0f60780abc2b06a13296d3712
Omar Morales <omarmoralesmm4533@gmail.com>
2024-05-04 09:04:11 -0500

    feat: add booking

--- src/public/router/router.js
+++ src/public/router/router.js
@@ -1,10 +1,14 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import HomePage from '../pages/home.page.vue';
 import VehiclesPage from '../../vehicle-management/pages/vehicles.page.vue';
+import ReservationPage from '../../booking-reservation/pages/reservation.page.vue';
+import ConfirmationPage from '../../booking-reservation/pages/confirmation.page.vue';
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/vehículos', component: VehiclesPage }
+  { path: '/vehículos', component: VehiclesPage },
+  { path: '/reservation', component: ReservationPage},
+  {path: '/confirmation',component: ConfirmationPage}
 ]
 
 const router = createRouter({
