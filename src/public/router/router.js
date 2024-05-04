fdf75b44c92fc91f97c5047af05097badcec227e
Milagros Tongo <107314451+Mily27@users.noreply.github.com>
2024-05-04 07:06:39 -0500

    feat: add registration confirmation

--- src/public/router/router.js
+++ src/public/router/router.js
@@ -4,11 +4,13 @@ import VehiclesPage from '../../vehicle-management/pages/vehicles.page.vue';
 import RatesPage from "../../user-management/pages/rates-page.vue";
 import RatesvPage from "../../user-management/pages/ratesv-page.vue";
 import ProfilePage from "../../user-management/pages/profile.page.vue";
+import ConfirmationRegisterPage from "../../user-management/pages/confirmation-register.page.vue";
 
 const routes = [
   { path: '/rates', component: RatesPage },
   { path: '/profile', component: ProfilePage},
-  { path: '/', component: RatesvPage }
+  { path: '/', component: RatesvPage },
+  {path: '/confirmationregister', component: ConfirmationRegisterPage}
 ]
 
 const router = createRouter({
