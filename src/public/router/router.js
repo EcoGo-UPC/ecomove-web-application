8369613f2a9f0279ad6762db118604be3728b48b
Milagros Tongo <107314451+Mily27@users.noreply.github.com>
2024-05-04 07:16:40 -0500

    feat: add reservation confirmation

--- src/public/router/router.js
+++ src/public/router/router.js
@@ -2,11 +2,13 @@ import { createWebHistory, createRouter } from 'vue-router';
 import HomePage from '../pages/home.page.vue';
 import VehiclesPage from '../../vehicle-management/pages/vehicles.page.vue';
 import ReservationPage from '../../booking-reservation/pages/reservation.page.vue';
+import ConfirmationPage from '../../booking-reservation/pages/confirmation.page.vue';
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/vehículos', component: VehiclesPage },
-  { path: '/reservation', component: ReservationPage}
+  { path: '/reservation', component: ReservationPage},
+  {path: '/confirmation',component: ConfirmationPage}
 ]
 
 const router = createRouter({
